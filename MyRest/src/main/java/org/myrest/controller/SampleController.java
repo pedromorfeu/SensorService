b8e30b43effc2477123c2f3d3f44f86f14257561
Pedro <pedromorfeu@gmail.com>
2016-05-12 17:52:51 +0200

    Hello

--- MyRest/src/main/java/org/myrest/controller/SampleController.java
+++ MyRest/src/main/java/org/myrest/controller/SampleController.java
@@ -25,7 +25,7 @@ public class SampleController {
 	@RequestMapping("/helloName")
 	@ResponseBody
 	String helloName(String name) {
-		return "Hello " + name;
+		return "Hello  " + name;
 	}
 
 	public static void main(String[] args) {
