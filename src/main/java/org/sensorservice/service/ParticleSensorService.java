8952fface77ca26f3446f872488cda16b9b8b631
Pedro <pedromorfeu@gmail.com>
2016-05-17 19:18:19 +0200

    Remove system outs

--- src/main/java/org/sensorservice/service/ParticleSensorService.java
+++ src/main/java/org/sensorservice/service/ParticleSensorService.java
@@ -1,7 +1,6 @@
 package org.sensorservice.service;
 
 import java.net.URISyntaxException;
-import java.util.Date;
 
 import javax.annotation.PostConstruct;
 
@@ -18,45 +17,47 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class ParticleSensorService implements SensorService {
 
-	private static final String URL = "https://api.particle.io/v1/devices/{device_id}/{variable}/?access_token={access_token}";
+  private static final String URL = "https://api.particle.io/v1/devices/{device_id}/{variable}/?access_token={access_token}";
 
-	private String deviceId;
-	private String accessToken;
+  private String deviceId;
+  private String accessToken;
 
-	@Autowired
-	private ApplicationArguments args;
+  @Autowired
+  private ApplicationArguments args;
 
-	@PostConstruct
-	private void setVariables() {
-		deviceId = args.getNonOptionArgs().get(1);
-		accessToken = args.getNonOptionArgs().get(2);
-	}
+  @PostConstruct
+  private void setVariables() {
+    deviceId = args.getNonOptionArgs()
+                   .get(1);
+    accessToken = args.getNonOptionArgs()
+                      .get(2);
+  }
 
-	@Cacheable(cacheManager = "cacheManager", cacheNames = { "temperature" })
-	public double getTemperature() throws RestClientException, URISyntaxException, InterruptedException {
-		System.out.println(new Date() + " getTemp");
-		RestTemplate restTemplate = new RestTemplate();
-		System.out.println("Making temperature request to Particle...");
-		String variable = "temp";
-		ResponseEntity<ParticleVariableResponse> response = restTemplate.exchange(URL, HttpMethod.GET, null, ParticleVariableResponse.class, deviceId, variable, accessToken);
-		return response.getBody().getResult();
-	}
+  @Cacheable(cacheManager = "cacheManager", cacheNames = { "temperature" })
+  public double getTemperature() throws RestClientException, URISyntaxException, InterruptedException {
+    RestTemplate restTemplate = new RestTemplate();
+    String variable = "temp";
+    ResponseEntity<ParticleVariableResponse> response = restTemplate.exchange(URL, HttpMethod.GET, null,
+        ParticleVariableResponse.class, deviceId, variable, accessToken);
+    return response.getBody()
+                   .getResult();
+  }
 
-	@Cacheable(cacheManager = "cacheManager", cacheNames = { "humidity" })
-	public double getHumidity() throws RestClientException, URISyntaxException, InterruptedException {
-		System.out.println(new Date() + " getTemp");
-		RestTemplate restTemplate = new RestTemplate();
-		System.out.println("Making humidity request to Particle...");
-		String variable = "humi";
-		ResponseEntity<ParticleVariableResponse> response = restTemplate.exchange(URL, HttpMethod.GET, null, ParticleVariableResponse.class, deviceId, variable, accessToken);
-		return response.getBody().getResult();
-	}
+  @Cacheable(cacheManager = "cacheManager", cacheNames = { "humidity" })
+  public double getHumidity() throws RestClientException, URISyntaxException, InterruptedException {
+    RestTemplate restTemplate = new RestTemplate();
+    String variable = "humi";
+    ResponseEntity<ParticleVariableResponse> response = restTemplate.exchange(URL, HttpMethod.GET, null,
+        ParticleVariableResponse.class, deviceId, variable, accessToken);
+    return response.getBody()
+                   .getResult();
+  }
 
-	public void setDeviceId(String deviceId) {
-		this.deviceId = deviceId;
-	}
+  public void setDeviceId(String deviceId) {
+    this.deviceId = deviceId;
+  }
 
-	public void setAccessToken(String accessToken) {
-		this.accessToken = accessToken;
-	}
+  public void setAccessToken(String accessToken) {
+    this.accessToken = accessToken;
+  }
 }
